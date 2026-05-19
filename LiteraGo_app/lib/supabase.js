58406e255eac948b6941f2dfe07442b5d94dcd1e
tsalmazahwa17 <tsalmazahwa@gmail.com>
2026-05-19 22:45:48 +0700

    Update supabase.js

--- LiteraGo_app/lib/supabase.js
+++ LiteraGo_app/lib/supabase.js
@@ -12,7 +12,7 @@ export const supabase = isSupabaseConfigured
       auth: {
         persistSession: true,
         autoRefreshToken: true,
-        detectSessionInUrl: true,
+        detectSessionInUrl: false,
         flowType: "pkce"
       }
     })
